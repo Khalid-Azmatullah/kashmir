76b0fb60655f26c82cab8d2bf12a7586cce4b44c
Khalid Azmatullah <khalid.studymaterial@gmail.com>
2024-10-07 20:13:03 +0530

    day-count

--- js/checkPass.js
+++ js/checkPass.js
@@ -3,13 +3,15 @@ function checkPass() {
   const correctPassword = "cockroach";
 
   if (passwordInput === correctPassword) {
-      window.location.href = "https://khalid-azmatullah.github.io/kashmir/dashboard.html";
+      window.location.href = "https://khalid-azmatullah.github.io/kashmir/redirect.html";
   } else {
       const errorSound = document.getElementById("errorSound");
       errorSound.play()
       document.getElementById("incorrectPasswordMessage").innerHTML = "Incorrect Password ❌";
       const message = document.getElementById("incorrectPasswordMessage");
       message.classList.add("incorrectAnimation");
-
+      setTimeout(() => {
+        message.classList.remove("incorrectAnimation");
+      }, 1000);
   }
 }
---
+++ js/redirect.js
@@ -0,0 +1,36 @@
+const currentDate = new Date();
+const month = currentDate.getMonth() + 1;
+const date = currentDate.getDate() + "-" + month + "-" + currentDate.getFullYear();
+
+console.log(date)
+
+const dayOne = "26-10-2024";
+const dayTwo = "27-10-2024";
+const dayThree = "28-10-2024";
+const dayFour = "29-10-2024";
+const dayFive = "30-10-2024";
+const daySix = "31-10-2024";
+const daySeven = "1-11-2024";
+const dayEight = "2-11-2024";
+const dayNine = "3-11-2024";
+
+
+if (date === dayOne) {
+    window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayOne.html";
+} else if (date === dayTwo) {
+    window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayTwo.html";
+} else if (date === dayThree) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayThree.html";
+} else if (date === dayFour) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayFour.html";
+} else if (date === dayFive) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayFive.html";
+} else if (date === daySix) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/daySix.html";
+} else if (date === daySeven) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/daySeven.html";
+} else if (date === dayEight) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayEight.html";
+} else if (date === dayNine) {
+  window.location.href = "https://khalid-azmatullah.github.io/kashmir/dayNine.html";
+}
