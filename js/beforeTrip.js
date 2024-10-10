91a61671b05393cd832ed63949137747e8b3ad5b
Khalid Azmatullah <khalid.studymaterial@gmail.com>
2024-10-10 15:24:35 +0530

    text-test-1

---
+++ js/beforeTrip.js
@@ -0,0 +1,11 @@
+/* const lastElementBg = document.querySelector(".headerBottom");
+
+window.addEventListener("scroll", checkBgView);
+
+function checkBgView() {
+  const triggerTop = window.innerHeight;
+
+  const elementTop = lastElementBg.getBoundingClientRect().top;
+
+  if (elementTop)
+}*/
