a1a59e74268e61d02cc85d03958d46eccd2e0034
Khalid Azmatullah <khalid.studymaterial@gmail.com>
2024-10-12 15:25:35 +0530

    flight-ticket-5

---
+++ js/flightTickets.js
@@ -0,0 +1,33 @@
+
+const pages = document.querySelectorAll(".page");
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+
+window.addEventListener("scroll", checkPages);
+
+function checkPages() {
+  const triggerWidth = window.innerWidth / 2;
+/*  
+  const pageOne = pages[0];
+  const pageRight1 = pageOne.getBoundingClientRect().left;
+  if (pageRight1 > triggerWidth) {
+    one.classList.add("active");
+  } else {
+    one.classList.remove("active");
+  }
+
+  
+  const pageTwo = pages[1];
+  const pageRight2 = pageOne.getBoundingClientRect().left;
+  if (pageRight2 < triggerWidth) {
+    two.classList.add("active");
+  } else {
+    two.classList.remove("active");
+  }
+*/
+
+}
